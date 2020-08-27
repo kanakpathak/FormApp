c27a2434eef0fb7b5e4d968105b1b5c15be4dd88
Kanak Pathak <kanakpathak1@gmail.com>
2020-08-28 00:03:58 +0530

    Fixed display data

--- src/employeeForm/displayData.js
+++ src/employeeForm/displayData.js
@@ -7,7 +7,7 @@ const DisplayData = () => {
   data = JSON.parse(data);
   return (
     <div className="formData">
-      {Object.keys(data).map((empid, index) => {
+      {Object.keys(data).map(empid => {
         return (
           <div
             key={empid}
@@ -25,32 +25,41 @@ const DisplayData = () => {
               </p>
             </div>
             <div>
-              <p>Name: {data[empid].name}</p>
+              <p>Name : {data[empid].name}</p>
             </div>
             <div>
-              <p>Designation: {data[empid].designation}</p>
+              <p>Designation : {data[empid].designation}</p>
             </div>
             <div style={{ display: "flex" }}>
-              <p>Contact: </p>
+              <p>Contact : </p>
               {data[empid].contact.map((info, index) => {
-                return (
-                  <p key={index}>
-                    {"  "}
-                    {info.contactType.toLowerCase()}-{info.contactNumber}
-                    {"  "}
+                return info.contactNumber !== "" ? (
+                  <p key={index} style={{ paddingLeft: "10px" }}>
+                    {info.contactType[0].toUpperCase()}
+                    {info.contactType.slice(1).toLowerCase()}
+                    {"-"}
+                    {info.contactNumber}
                   </p>
+                ) : (
+                  ""
                 );
               })}
             </div>
             <div style={{ display: "flex" }}>
-              <p>Skills: </p>
+              <p>Skills : </p>
               {data[empid].skills.map((skill, index) => {
-                return index === data[empid].skills.length - 1 ? (
-                  <p>{skill} </p>
+                return skill !== "" ? (
+                  <p key={index} style={{ paddingLeft: "10px" }}>
+                    {skill}
+                    {index === data[empid].skills.length - 1 ? "" : ", "}
+                  </p>
                 ) : (
-                  <p>{skill}, </p>
+                  ""
                 );
               })}
+            </div>
+            <div>
+              <p>Date of Birth : {data[empid].dob}</p>
             </div>
             <br />
             <br />
